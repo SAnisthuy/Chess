8ae88b55db20790e453392ced6ed594cdcd08430
SAnisthuy <sansithu215@gmail.com>
2026-02-03 19:00:13 +1300

    Added capturing and fixed bugs

--- new.py
+++ new.py
@@ -1,89 +1,111 @@
 from tkinter import *
 
-grid = []
-select = None
 
+class Chess:
+    def __init__(self):
+        self.grid = []
+        self.select = None
+        self.prev = None
 
-for i in range(8):
-    row = []
-    for i in range(8):
-        row.append(0)
-    grid.append(row)
+    def update_button(self, row, col, **kwargs):
+        widgets = self.main.grid_slaves(row=row, column=col) 
+        if widgets:
+            btn = widgets[0]
+            btn.config(**kwargs)
 
-# rooks
-grid[0][0], grid[0][7] = 14, 14
-grid[7][0], grid[7][7] = 4, 4
+    def on_click(self, y, x):
+        col = "gray" if (y + x) % 2 else "white"
+        if self.select != None: # Selects an empty square with something previously selected
 
-# knights
-grid[0][1], grid[0][6] = 13, 13
-grid[7][1], grid[7][6] = 3, 3
+            self.grid[y][x] = self.select
+            self.select = None
+            self.update_button(y, x, image=self.pieces[self.grid[y][x]])
+            self.update_button(self.prev[0], self.prev[1], image=self.pieces[0], bg=self.prev[2])
+            self.prev =None
 
-# bishops
-grid[0][2], grid[0][5] = 12, 12
-grid[7][2], grid[7][5] = 2, 2
+        elif self.select == None and self.grid[y][x] != 0: # Selects a non-empty square with nothing previously selected
 
-# king and queens
-grid[0][4], grid[7][4] = 16, 6
-grid[0][3], grid[7][3] = 15, 5
+            self.select = self.grid[y][x]
+            self.update_button(y, x, bg="light green")
+            self.prev = (y, x, col)
 
-# pawns
-for i in range(8):
-    grid[1][i] = 11
-    grid[6][i] = 1
-
-
-def on_click(y, x):
-    global select
-    if grid[y][x] == 0 and select != None:
-        grid[y][x] = select
-        select = None
-    elif select == None and grid[y][x] != 0:
-        select = grid[y][x]
-        
-    else:
-        print("Invalid interaction")
+        else: # Neither
+            print("Invalid interaction")
     
-main = Tk()
+    def set_up(self):
+        # Load pieces
+        self.main.darkRook   = PhotoImage(file=r"DarkRook.png").subsample(6, 6)
+        self.main.darkKnight = PhotoImage(file=r"DarkKnight.png").subsample(6, 6)
+        self.main.darkBishop = PhotoImage(file=r"DarkBishop.png").subsample(6, 6)
+        self.main.darkQueen  = PhotoImage(file=r"DarkQueen.png")
+        self.main.darkKing   = PhotoImage(file=r"DarkKing.png").subsample(6, 6)
+        self.main.darkPawn   = PhotoImage(file=r"DarkPawn.png").subsample(6, 6)
 
-# Load pieces
-main.darkRook   = PhotoImage(file=r"DarkRook.png").subsample(6, 6)
-main.darkKnight = PhotoImage(file=r"DarkKnight.png").subsample(6, 6)
-main.darkBishop = PhotoImage(file=r"DarkBishop.png").subsample(6, 6)
-main.darkQueen  = PhotoImage(file=r"DarkQueen.png")
-main.darkKing   = PhotoImage(file=r"DarkKing.png").subsample(6, 6)
-main.darkPawn   = PhotoImage(file=r"DarkPawn.png").subsample(6, 6)
+        self.main.whiteRook   = PhotoImage(file=r"LightRook.png").subsample(6, 6)
+        self.main.whiteBishop = PhotoImage(file=r"LightBishop.png").subsample(6, 6)
+        self.main.whiteKnight = PhotoImage(file=r"LightKnight.png").subsample(6, 6)
+        self.main.whiteQueen  = PhotoImage(file=r"LightQueen.png").subsample(4, 4)
+        self.main.whiteKing   = PhotoImage(file=r"LightKing.png").subsample(6, 6)
+        self.main.whitePawn   = PhotoImage(file=r"LightPawn.png").subsample(6, 6)
 
-main.whiteRook   = PhotoImage(file=r"LightRook.png").subsample(6, 6)
-main.whiteBishop = PhotoImage(file=r"LightBishop.png").subsample(6, 6)
-main.whiteKnight = PhotoImage(file=r"LightKnight.png").subsample(6, 6)
-main.whiteQueen  = PhotoImage(file=r"LightQueen.png").subsample(4, 4)
-main.whiteKing   = PhotoImage(file=r"LightKing.png").subsample(6, 6)
-main.whitePawn   = PhotoImage(file=r"LightPawn.png").subsample(6, 6)
+        self.main.blank = PhotoImage(file=r"Blank.png")
 
-main.blank = PhotoImage(file=r"Blank.png")
+        # map
+        self.pieces = {0: self.main.blank, 
+                1: self.main.whitePawn, 
+                2: self.main.whiteBishop, 
+                3: self.main.whiteKnight, 
+                4: self.main.whiteRook, 
+                5: self.main.whiteQueen,
+                6: self.main.whiteKing, 
+                11: self.main.darkPawn,
+                12: self.main.darkBishop,
+                13: self.main.darkKnight,
+                14: self.main.darkRook,
+                15: self.main.darkQueen,
+                16: self.main.darkKing}
 
-# map
-pieces = {0: main.blank, 
-          1: main.whitePawn, 
-          2: main.whiteBishop, 
-          3: main.whiteKnight, 
-          4: main.whiteRook, 
-          5: main.whiteQueen,
-          6: main.whiteKing, 
-          11: main.darkPawn,
-          12: main.darkBishop,
-          13: main.darkKnight,
-          14: main.darkRook,
-          15: main.darkQueen,
-          16: main.darkKing}
+        for i in range(8):
+            row = []
+            for i in range(8):
+                row.append(0)
+            self.grid.append(row)
+
+        # rooks
+        self.grid[0][0], self.grid[0][7] = 14, 14
+        self.grid[7][0], self.grid[7][7] = 4, 4
+
+        # knights
+        self.grid[0][1], self.grid[0][6] = 13, 13
+        self.grid[7][1], self.grid[7][6] = 3, 3
+
+        # bishops
+        self.grid[0][2], self.grid[0][5] = 12, 12
+        self.grid[7][2], self.grid[7][5] = 2, 2
+
+        # king and queens
+        self.grid[0][4], self.grid[7][4] = 16, 6
+        self.grid[0][3], self.grid[7][3] = 15, 5
+
+        # pawns
+        for i in range(8):
+            self.grid[1][i] = 11
+            self.grid[6][i] = 1
+
+    def run(self):
+    
+        self.main = Tk()
+
+        self.set_up()
+        for i in range(8):
+            for j in range(8):
+                color = "gray" if (i + j) % 2 else "white"
+                btn = Button(self.main, image= self.pieces[self.grid[i][j]], command=lambda r=i, c=j: self.on_click(r, c))
+                btn.config(bg=color)
+                btn.grid(row=i, column=j, sticky="nsew")
+
+        mainloop()
 
 
-# Tkinter
-
-for i in range(8):
-    for j in range(8):
-        color = "gray" if (i + j) % 2 else "white"
-        btn = Button(main, bg=color, image= pieces[grid[i][j]], command=lambda r=i, c=j: on_click(r, c))
-        btn.grid(row=i, column=j, sticky="nsew")
-
-mainloop()
+run = Chess()
+run.run()
